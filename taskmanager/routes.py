c0d6a99d04bbda94bdc0eca62bdaa29ad8aab6f9
SkyeH-M <justskye98@gmail.com>
2023-03-26 12:50:14 +0000

    Initialising modal for task deletion

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,7 +10,8 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
@@ -21,3 +22,28 @@ def add_category():
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    # query db, try to find specified record using data provided
+    # if no match is found, return 404 error page
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        # update category_name for our cat variable, set it equal to forms
+        # name-attribute of category_name
+        category.category_name = request.form.get("category_name")
+        # commit session to db
+        db.session.commit()
+        # if all successful, redirect back to categories func which
+        # displays cards again
+        return redirect(url_for("categories"))
+    return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+
+// modal initialisation
+let modals = document.querySelectorAll('.modal');
+M.Modal.init(modals);
+});
