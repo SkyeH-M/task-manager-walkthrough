32968a0cf30f05dea4a27b9a5ef6568388918798
SkyeH-M <justskye98@gmail.com>
2023-03-27 11:47:28 +0000

    Added Tasks display

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidenav initialisation
-    let sidenav = document.querySelectorAll('.sidenav');
+document.addEventListener("DOMContentLoaded", function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
-// modal initialisation
-let modals = document.querySelectorAll('.modal');
-M.Modal.init(modals);
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // modal initialization
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    // collapsible initialisation
+    let collapsibles = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsibles);
 });
